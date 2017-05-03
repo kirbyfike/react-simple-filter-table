5250e5bd46e79aa93507224b55210ebf56878da6
Kirby Fike <kirbyfike@gmail.com>
2017-05-03 15:36:13 -0700

    first

---
+++ README.md
@@ -0,0 +1,5 @@
+<div align="center">
+ <h1>React Simple Filter Table</h1>
+</div>
+## Author
+Kirby Fike
---
+++ src/Filter.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Filter = props => (
+  <h1>Hello</h1>
+);
+
+export default Filter;
